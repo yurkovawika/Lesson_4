56bb0d806d7595547c2fe99e6e4d480a55f01276
Viktorinkiss <yurkovawika@gmail.com>
2022-07-06 21:20:09 +0500

    Creating file Work_3

---
+++ Work_3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] myArray = {1,4,2,7,4,9,2,5};
+void PrintArray(int [] array)
+{
+    Console.Write("[ ");
+    Console.Write($"{array[0]}");
+    int count = array.Length;
+    for(int i = 1; i < count;i++)
+    {
+        Console.Write(", " + array[i]);
+    }
+    Console.WriteLine(" ]");
+}
+
+PrintArray(myArray);
